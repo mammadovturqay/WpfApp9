18f0093f0c269cfcc4535611cd4be98bdc08ca99
memme_oy59 <memme_oy59@STHQ0125-09>
2023-10-24 15:06:36 +0400

    Add project files.

---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            string message = messageTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                // Kullanıcının gönderdiği mesajı ListBox'a ekleyin
+                messageListBox.Items.Add("Men: " + message);
+
+                // Burada mesajı alıcıya göndermek için gerekli işlemleri yapabilirsiniz.
+
+                // Mesaj gönderildikten sonra TextBox'ı temizleyin
+                messageTextBox.Clear();
+            }
+        }
+    }
+}
